56f48817840ac0aa3cde8fce369f2142cfa8a998
Andrew Azores <me@andrewazor.es>
2015-02-13 14:42:01 -0500

    Initial commit

---
+++ eclipseproject/src/ca/team3161/fenrir/Robot.java
@@ -0,0 +1,38 @@
+
+package ca.team3161.fenrir;
+
+import ca.team3161.lib.robot.TitanBot;
+
+public class Robot extends TitanBot {
+
+    @Override
+    public void autonomousRoutine() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getAutonomousPeriodLengthSeconds() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void robotInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void teleopInit() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void teleopRoutine() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
